35c53b434111ef3dc1c248b498699e3f0a577227
bella <1596867394@qq.com>
2017-11-10 17:07:41 +0800

    表单验证完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,7 +1,18 @@
 //验证是否登录
 
 console.log(location.href);
-
+if((location.href).indexOf('login.html')==-1){
+    $.ajax({
+        url:'/employee/checkRootLogin',
+        type:'get',
+        success:function(data){
+            console.log(data);
+            if(data.error==400){
+                location.href='login.html'
+            }
+        }
+    })
+}
 
 
 
