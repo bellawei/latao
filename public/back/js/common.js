2ebcfdb9d7e7c870ef18ae9e340740b7382b0443
bella <1596867394@qq.com>
2017-11-10 21:22:11 +0800

    完成一级分类

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,14 +1,13 @@
 //验证是否登录
 
 console.log(location.href);
-if((location.href).indexOf('login.html')==-1){
+if ((location.href).indexOf('login.html') == -1) {
     $.ajax({
-        url:'/employee/checkRootLogin',
-        type:'get',
-        success:function(data){
-            console.log(data);
-            if(data.error==400){
-                location.href='login.html'
+        url: '/employee/checkRootLogin',
+        type: 'get',
+        success: function (data) {
+            if (data.error == 400) {
+                location.href = 'login.html'
             }
         }
     })
@@ -16,13 +15,30 @@ if((location.href).indexOf('login.html')==-1){
 
 
 
-$('#hideaside').on('click',function(){
+$('#hideaside').on('click', function () {
     $('.aside').toggleClass("move");
     $('.main').toggleClass("move");
 })
-    
-$('#catergory').on('click',function(){
+
+$('#catergory').on('click', function () {
     $('.child').slideToggle();
 })
 
+$('#logout').on('click', function () {
+    $('#logoutmodel').modal('show');
+    
+    $('#confirmlogout').off().on('click', function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            success: function (data) {
+
+                if (data.success) {
+                    location.href = "login.html";
+                }
+            }
+        });
+    })
+
+})
 
