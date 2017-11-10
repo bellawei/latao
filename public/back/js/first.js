3958ce99d15d96f4c6e13f803735d2fca72edf8f
bella <1596867394@qq.com>
2017-11-11 01:08:29 +0800

    first页面完成表单校验

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -11,11 +11,10 @@ $(function(){
                 pageSize:pageSize
             },
             success:function(data){
-                console.log(data);
                 $('tbody').html(template('tpl',data));
 
                 $('#paginator').bootstrapPaginator({
-                    bootstrapMajorVersion:3,
+                    bootstrapMajorVersion: 3,
                     currentPage:currentPage,
                     totalPages:Math.ceil(data.total/pageSize), 
                     onPageClicked:function(a,b,c,page){
@@ -23,13 +22,56 @@ $(function(){
                         render();
                     }
                 })
+                
               
             }
         })  
     }
     render();
 
-    
+    $('.contentbox button').on('click',function(){
+        $('#addcategory').modal('show');
+    })
+
+    var $form=$('form');
+    console.log($form);
+    $form.bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+        fields:{
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入一级分类"
+                    }
+                }
+            }
+        }
+    })
+
+    $form.on('success.form.bv',function(e){
+        e.preventDefault();
+
+        $.ajax({
+            type:'post',
+            url:'/category/addTopCategory',
+            data:$form.serialize(),
+            success:function(data){
+                console.log(data);
+                if(data.success){
+                    $("#addcategory").modal("hide");
+                    currentPage = 1;
+                    render();
+
+                    $form.data("bootstrapValidator").resetForm();
+                    $form[0].reset();
+                }
+            }
+        })
+    })
 
 
 
